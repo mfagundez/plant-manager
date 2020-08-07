3d2492430c092bcd18c3f2275906bd4ab1f5aa4a
Mario Fagúndez Rodríguez <mfagundez@corp.tecsisa.com>
2020-08-07 14:11:28 +0200

    print

--- plant-mgr/plantmgr.py
+++ plant-mgr/plantmgr.py
@@ -4,3 +4,5 @@ from miflora.miflora_poller import MiFloraPoller
 from btlewrap.bluepy import BluepyBackend
 
 poller = MiFloraPoller('some mac address', BluepyBackend)
+
+print("hola miFlora. El nivel de bateria es: " + poller.battery_level)
